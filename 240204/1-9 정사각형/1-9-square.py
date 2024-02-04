e3c0c5891afb4af963dec588785348243e18cf50
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-02-04 21:51:01 +0900

    Title: 1-9 정사각형, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/1-9 정사각형/1-9-square.py
+++ 240204/1-9 정사각형/1-9-square.py
@@ -3,6 +3,11 @@ cnt = 1
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        print(cnt, end = "")
-        cnt += 1
+        if cnt == 10:
+            cnt =1
+            print(cnt, end = "")
+            cnt += 1
+        else:
+            print(cnt, end = "")
+            cnt += 1
     print()
