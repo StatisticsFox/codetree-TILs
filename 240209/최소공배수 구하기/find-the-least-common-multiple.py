731df2eee296646d4d4e0d8501462dcf324c6ab3
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-02-09 22:16:18 +0900

    Title: 최소공배수 구하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240209/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,9 +1,9 @@
 n, m = tuple(map(int, input().split()))
 
 def min_eq(n, m):
-    for i in range(1, max(n, m)):
-        h = min(n, m)
-        if (h*i)%n==0 and (h*i)%m==0:
-            print(h*i); break        
+    for i in range(max(n, m), (n*m)+1):
+        if i%n==0 and i%m==0:
+            print(i)
+            break
             
 min_eq(n, m)
