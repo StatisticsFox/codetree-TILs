1a33df2eaa76bd67ba1ca57d27c43fc5d4834cd2
CHOI_JEE_HYUK <92065443+StatisticsFox@users.noreply.github.com>
2024-02-09 22:27:34 +0900

    Title: 최대공약수 구하기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240209/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -23,3 +23,20 @@ def 최대공약수(n, m):
         print(max(result))
 
 최대공약수(n, m)
+
+## 이상적인 코드
+# # 변수 선언 및 입력:
+# n, m = tuple(map(int, input().split()))
+
+
+# # n과 m의 최대공약수를 반환합니다.
+# def find_gcd(n, m):
+#     gcd = 0
+#     for i in range(1, min(n, m) + 1):
+#         if n % i == 0 and m % i == 0:
+#             gcd = i
+
+#     print(gcd)
+
+
+# find_gcd(n, m)
